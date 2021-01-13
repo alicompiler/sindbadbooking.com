fca4c74ce006db7f83088f07d1d3fd7ef2e6abd9
Ali Faris <alifaris@earthlink.iq>
2021-01-13 14:40:00 +0300

    add world background and fixing .gitignore cache

--- src/Pages/Index/Components/Landing/LandingPage.tsx
+++ src/Pages/Index/Components/Landing/LandingPage.tsx
@@ -12,9 +12,11 @@ export class LandingPage extends React.Component<Props> {
 
     render(): any {
         const {appDescriptor} = this.props;
-        return <div className={'w-screen overflow-hidden relative'}>
+        return <div className={'w-screen overflow-hidden relative h-full'}>
             <MainContent appDescriptor={appDescriptor}/>
-            <div className={'bg-black opacity-75 absolute top-0 left-0 w-full h-full z-20'}/>
+            <div className={'bg-black opacity-75 absolute top-0 left-0 w-full h-full z-20'} style={{
+                background: `url(/res/bg/world.jpg)`
+            }}/>
         </div>
     }
 
--- src/Pages/Index/Components/RegistrationSection/RegistrationSection.tsx
+++ src/Pages/Index/Components/RegistrationSection/RegistrationSection.tsx
@@ -7,7 +7,7 @@ class RegistrationSection extends Component {
     render() {
         return (
             <section className={'bg-gray-700 p-4 md:p-8 lg:p-16 xl:p-24'}>
-                <div className={"mx-auto container"}>
+                <div id={"register"} className={"mx-auto container"}>
                     <SectionTitle title={getLocals().register}/>
                     <RegistrationForm/>
                 </div>
